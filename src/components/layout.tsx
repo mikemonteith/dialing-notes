c588de0a84eb73a874ea1e1edce7dc086ec0818a
Mike Monteith <mike@mikemonteith.com>
2023-01-19 14:06:23 +0000

    Remove nextjs defaults and begin layout

---
+++ src/components/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from '@/styles/Layout.module.css'
+
+export default function Layout({ children } : {children: React.ReactNode}) {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className="container">
+            <div className={styles.logo}>Tasting Notes</div>
+        </div>
+      </header>
+      <main className={'container ' + styles.main}>{children}</main>
+      <footer></footer>
+    </>
+  )
+}
