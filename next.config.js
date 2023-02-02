2108a1cae22a75f6987f6594f40bf69f36900c85
Mike Monteith <mike@mikemonteith.com>
2023-02-02 13:11:24 +0000

    Add a sitemap rewrite to example.com

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return {
+      beforeFiles: [{
+        source: '/sitemap/:path*',
+        destination: 'https://example.com/:path',
+      }]
+    }
+  }
 }
 
 module.exports = nextConfig
