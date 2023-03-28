9109fd2c39d67ebe4fa8b44d9f1b01a2ec3dc1c6
Mike Monteith <mike@mikemonteith.com>
2023-03-28 17:08:18 +0100

    Add custom domain to cloudfront

--- infra/init.ts
+++ infra/init.ts
@@ -1,16 +1,34 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { WebAppStack } from './stack';
+import { WebAppStack } from './web-app-stack';
+import { CertificateStack } from './certificate-stack';
 
 const app = new cdk.App();
+
+const accountId = '488860341912'
+
+const tags = {
+  project: 'Dialing Notes'
+}
+
+// The certificate must be in us-east-1 for use with cloudfront
+const certificateStack = new CertificateStack(app, 'DialingNotesCertificate', {
+  env: {
+    account: accountId,
+    region: 'us-east-1'
+  },
+  crossRegionReferences: true,
+  tags,
+})
+
 new WebAppStack(app, 'DialingNotesWebApp', {
   env: {
-    account: '488860341912',
+    account: accountId,
     region: 'eu-west-1'
   },
-
-  // Add project tags to every resource in the stack
-  tags: {
-    project: 'Dialing Notes'
-  }
+  crossRegionReferences: true,
+  certificateArn: certificateStack.certificateArn,
+  zoneId: certificateStack.zoneId,
+  zoneName: certificateStack.zoneName,
+  tags,
 });
--- infra/web-app-stack.ts
+++ infra/web-app-stack.ts
@@ -1,14 +1,25 @@
-import * as path from 'path'
-
 import * as cdk from 'aws-cdk-lib'
 import * as s3 from 'aws-cdk-lib/aws-s3'
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as route53 from 'aws-cdk-lib/aws-route53'
 import { Construct } from 'constructs'
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager'
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets'
+
+
+interface WebAppStackProps extends cdk.StackProps {
+  certificateArn: string;
+  zoneId: string;
+  zoneName: string;
+}
+
 
 export class WebAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: WebAppStackProps) {
     super(scope, id, props)
+
+    const { certificateArn, zoneId, zoneName } = props
 
     const bucket = new s3.Bucket(this, 'WebsiteBucket', {
       versioned: false,
@@ -38,6 +49,8 @@ export class WebAppStack extends cdk.Stack {
       `)
     })
 
+    const certificate = Certificate.fromCertificateArn(this, 'Certificate', certificateArn)
+
     const distribution = new cloudfront.CloudFrontWebDistribution(this, 'WebsiteDistribution', {
       defaultRootObject: 'index.html',
       originConfigs: [
@@ -64,6 +77,19 @@ export class WebAppStack extends cdk.Stack {
           responsePagePath: '/404.html',
         },
       ],
+      viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(certificate, {
+        aliases: [zoneName],
+        securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+      }),
+      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    })
+
+    new route53.ARecord(this, 'Alias', {
+      zone: route53.HostedZone.fromHostedZoneAttributes(this, 'Zone', {
+        zoneName: zoneName,
+        hostedZoneId: zoneId,
+      }),
+      target: route53.RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
     })
 
     new cdk.CfnOutput(this, 'WebsiteBucketNameOutput', {
